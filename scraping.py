facb6d4e8d77d7aa7e4928eda3f99865b583d9cb
Esmerlin <2025-0281@unad.edo.do>
2025-12-11 19:50:10 -0400

    Modificado

--- scraping.py
+++ scraping.py
@@ -1,13 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
+import time
 
 url = "https://quotes.toscrape.com/"
 response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
 
-quote = soup.find_all("div", class_="quote")
+quotes = soup.find_all("div", class_="quote")
 
+for q in quotes:
+    texto = q.find("span", class_="text").text.strip()
+    autor = q.find("small", class_="author").text.strip()
+    tags = q.find_all("a", class_="tag")
 
-for q in quote:
-    print(q.text)
+    print("Texto:", texto)
+    print("Autor:", autor)
+
+    for t in tags:
+        print("Tag:", t.text)
+
+    print("-" * 40)
+    time.sleep(1)
+
 
