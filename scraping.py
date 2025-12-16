04e9046f7e45d910dde1150471c3e1b324aa1fd9
Esmerlin <2025-0281@unad.edo.do>
2025-12-15 20:10:31 -0400

    Fixeado

--- scraping.py
+++ scraping.py
@@ -7,12 +7,18 @@ response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
 
 quotes = soup.find_all("div", class_="quote")
+titulo = soup.find("div", class_="col-md-8").text
+top_tags = soup.find_all("span", class_="tag-item")
 
-for q in quotes:
+print("Titulo:", titulo)
+
+for q in quotes[1:6:3]:
+
     texto = q.find("span", class_="text").text.strip()
     autor = q.find("small", class_="author").text.strip()
     tags = q.find_all("a", class_="tag")
 
+    
     print("Texto:", texto)
     print("Autor:", autor)
 
@@ -20,6 +26,10 @@ for q in quotes:
         print("Tag:", t.text)
 
     print("-" * 40)
+    time.sleep(1)
+
+for o in top_tags:
+    print("Tags:", o.text)
     time.sleep(1)
 
 
